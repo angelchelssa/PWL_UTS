db685efae7ddd61b6c3c127c0231ed5ed16bb867
angelchelssa <angelchelsa150526@gmail.com>
2026-04-17 19:03:44 +0700

    membuat model

---
+++ app/Models/Penjualan.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\PenjualanDetail;
+
+class Penjualan extends Model
+{
+    protected $table = 't_penjualan';
+    protected $primaryKey = 'penjualan_id';
+    protected $fillable = [
+        'user_id',
+        'pembeli',
+        'penjualan_kode',
+        'penjualan_tanggal',
+    ];
+
+    protected $casts = [
+        'penjualan_tanggal' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function detail()
+    {
+        return $this->hasMany(PenjualanDetail::class, 'penjualan_id', 'penjualan_id');
+    }
+
+    public function getTotalAttribute()
+    {
+        return $this->detail->sum(fn($d) => $d->harga * $d->jumlah);
+    }
+}
